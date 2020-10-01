0e78c3f69c67103b2d86ff9c6791468af03e0ed7
Paul Ngisiro <jngisiro@gmail.com>
2020-10-01 21:30:22 +0300

    :pushpin: feat: added register and login components

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { NavComponent } from './nav/nav.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { FooterComponent } from './footer/footer.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [NavComponent, SidebarComponent, FooterComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   exports: [NavComponent, SidebarComponent, FooterComponent],
 })
 export class SharedModule {}
