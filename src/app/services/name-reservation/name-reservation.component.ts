7261f7c06c4bd9a919aa8ffc4369206adb433fb2
Paul Ngisiro <jngisiro@gmail.com>
2020-10-04 18:01:22 +0300

    :pushpin: feat: added name reservation component

---
+++ src/app/services/name-reservation/name-reservation.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-name-reservation',
+  templateUrl: './name-reservation.component.html',
+  styleUrls: ['./name-reservation.component.scss'],
+})
+export class NameReservationComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
--- src/app/services/services-routing.module.ts
+++ src/app/services/services-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AuditingComponent } from './auditing/auditing.component';
 import { IndexComponent } from './index/index.component';
+import { NameReservationComponent } from './name-reservation/name-reservation.component';
 import { NameSearchComponent } from './name-search/name-search.component';
 import { RegistrationComponent } from './registration/registration.component';
 
@@ -21,6 +22,10 @@ const routes: Routes = [
   {
     path: 'auditing',
     component: AuditingComponent,
+  },
+  {
+    path: 'name-reservation',
+    component: NameReservationComponent,
   },
 ];
 
